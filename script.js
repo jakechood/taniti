de836c6698ef9e4907ecd8f49692793364f238a0
Jake Sturgill Hood <jakechood@gmail.com>
2024-06-26 08:27:45 -0500

    Connects jquery nav scroll effect API to target elements

--- script.js
+++ script.js
@@ -27,31 +27,31 @@ function onClick(element) {
       }
   }
 
-  $('#sec-1').addClass("active");
+  $('#home').addClass("active");
 
 //   Smooth scrolling
-      $("#sec-1").click(function() {
+      $("#home").click(function() {
            $('html, body').animate({
                scrollTop:        $("#one").offset().top-66
            }, 1000);
         return false;
        });
       
-      $("#sec-2").click(function() {
+      $("#localLiving").click(function() {
            $('html, body') .animate({
                scrollTop:        $("#two").offset().top-112
            }, 1000);
         return false;
        });
       
-      $("#sec-3").click(function() {
+      $("#transportation").click(function() {
            $(' html,body') .animate({
                scrollTop:        $("#three").offset().top-112
            }, 1000);
         return false;
        });
       
-      $("#sec-4").click(function() {
+      $("#entertainment").click(function() {
         $(this).addClass("active");
            $('html,body ') .animate({
                scrollTop:        $("#four").offset().top-112
@@ -59,25 +59,38 @@ function onClick(element) {
         return false;
        });
 
+       $("#faq").click(function() {
+        $(this).addClass("active");
+           $('html,body ') .animate({
+               scrollTop:        $("#five").offset().top-112
+           }, 1000);
+        return false;
+       });
+
        $('#two').waypoint(function() {
       
         $(".container ul li").children().removeClass("active");
-        $("#sec-2").addClass("active");
+        $("#localLiving").addClass("active");
         
       }, { offset: 101 });
       
       
       $('#three').waypoint(function() {
         $(".container ul li").children().removeClass("active");
-        $("#sec-3").addClass("active");
+        $("#transportation").addClass("active");
       }, { offset: 101 });
       
       $('#four').waypoint(function() {
         $(".container ul li").children().removeClass("active");
-        $("#sec-4").addClass("active");
+        $("#entertainment").addClass("active");
+      }, { offset: 101 });
+
+      $('#five').waypoint(function() {
+        $(".container ul li").children().removeClass("active");
+        $("#faq").addClass("active");
       }, { offset: 101 });
       
       $('#one').waypoint(function() {
         $(".container ul li").children().removeClass("active");
-        $("#sec-1").addClass("active");
+        $("#home").addClass("active");
       }, { offset: 0 });
